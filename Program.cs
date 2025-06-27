ab59dcc3016c6b427c9c31d2758a54c010e72942
Jurandi Junior <jurandijr20@gmail.com>
2025-06-27 18:42:56 -0300

    Introdução a delegates

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
-﻿internal class Program
+﻿using POO_CSharp_P28.Services;
+
+delegate double BinaryNumericOperation(double n1, double n2);
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+
+        double a = 10;
+        double b = 12;
+
+        BinaryNumericOperation op = CalculationService.Max;
+
+        double result = op(a, b);
+        Console.WriteLine(result);
     }
 }
